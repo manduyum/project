9e8cff150c9d6d1bef258859659657215d9e70f9
manduyum <163153544+manduyum@users.noreply.github.com>
2024-03-17 09:25:49 +0900

    project c++ programming
    project c++ programming

---
+++ Project1/Source02.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//using namespace std; 
+////std 네임스페이스를 사용하고 있습니다. 라는 뜻 
+////namespace란? 별도의 이름을 저장할 수 있는 공간
+////사용목적 1)프로그램을 개발하다 보면 코드를 특정 그룹으로 분리하고 싶은 경우
+////	2)프로그램에서 자주 쓰이는 변수의 이름이 충돌되지 않게 하기 위함.
+//
+//// 네임 스페이스는 중첩해서 사용 가능.
+//namespace Item {
+//	namespace Cloth {
+//		string brand = "Nike";
+//	}
+//
+//}
+//
+//namespace A {
+//	int a = 1; // 정수형 변수 a
+//	//변수(variable) : 프로그램에서 사용할 데이터 형태에 이름을 붙인 것으로, 하나의 데이터를 저장한다.
+//	//정수형 : 소수점이 없는 숫자를 의미.
+//}
+//namespace B {
+//	int a = 2;
+//}
+//
+//int main()
+//{
+//	cout << "안녕하세요." << endl;
+//	int a = 5;
+//	cout << a << endl;
+//	cout << A::a << endl;
+//	cout << B::a << endl;
+//	cout << Item::Cloth::brand << endl;
+//	
+//	return 0; // 함수를 종료하고 0을 전달합니다.
+//}
+
+
